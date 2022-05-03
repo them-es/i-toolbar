9083582a9b8689a19c3942461561827686b5492c
them-es <them-es@users.noreply.github.com>
2022-05-03 10:21:47 +0200

    Update toolbar icon style

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-rich-text'), 'version' => '2348b522b4a0e14e213f7f08b0dba348');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'd23a5563a6806e08b2de1a2cd203fe92');
